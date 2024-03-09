2e22105b0bae9faada73ed329e51431a511d426d
Shivam Pandey <shivamkumarpandey3971@gmail.com>
2024-03-09 18:36:53 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
 import express from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
+import cors from "cors";
 
 const port = 3000;
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
-const io = new Server(server, {});
+// const io = new Server(server, {});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
@@ -18,6 +34,6 @@ io.on("connection", (socket) => {
   console.log("Id", socket.id);
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
